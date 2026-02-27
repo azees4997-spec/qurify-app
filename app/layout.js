6b8410eda54d094164a43da769554317399b59b1
azees4997-spec <azees4997@gmail.com>
2026-02-28 04:33:01 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,36 @@
 "use client";
 
-import { CartProvider } from "./context/CartContext";
 import "./globals.css";
+import Link from "next/link";
+import { CartProvider } from "./context/CartContext";
+import { useCart } from "./context/CartContext";
+
+function Navbar() {
+  const cartContext = useCart();
+  const count = cartContext?.cart?.length || 0;
+
+  return (
+    <div className="navbar">
+      <Link href="/" className="logo">Qurify</Link>
+
+      <div className="nav-right">
+        <Link href="/catalogue" className="nav-link">Medicines</Link>
+
+        <Link href="/cart" className="cart-icon">
+          🛒
+          {count > 0 && <span className="cart-count">{count}</span>}
+        </Link>
+      </div>
+    </div>
+  );
+}
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html>
       <body>
         <CartProvider>
+          <Navbar />
           {children}
         </CartProvider>
       </body>
