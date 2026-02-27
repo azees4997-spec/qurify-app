b418b035d771766b94c929492ccb14b3d5396f55
azees4997-spec <azees4997@gmail.com>
2026-02-28 01:48:27 +0530

    Create page.js

---
+++ app/order-success/page.js
@@ -0,0 +1,8 @@
+export default function Success() {
+  return (
+    <div className="container">
+      <h2>Order Placed Successfully!</h2>
+      <p>Thank you for shopping with Qurify.</p>
+    </div>
+  );
+}
