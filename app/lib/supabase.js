acc4bc6fc4f9a13300b10451811a2899fb2d2e0c
azees4997-spec <azees4997@gmail.com>
2026-02-28 01:32:51 +0530

    Update supabase.js

--- app/lib/supabase.js
+++ app/lib/supabase.js
@@ -3,4 +3,4 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(https://rhxrtsjocqizhxqhbzgu.supabase.co, eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJoeHJ0c2pvY3Fpemh4cWhiemd1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEzMjY1MzQsImV4cCI6MjA4NjkwMjUzNH0.fa6pG3IxS05ngcpbrWCWPA5NAYF1YQ2kRn9WO__81qM);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
