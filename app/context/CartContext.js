ee904b1e942e176e51140c0a98ed8c2dc26ad393
azees4997-spec <azees4997@gmail.com>
2026-02-28 01:43:36 +0530

    Implement CartContext with add, remove, and clear functions

---
+++ app/context/CartContext.js
@@ -0,0 +1,31 @@
+"use client";
+
+import { createContext, useContext, useState } from "react";
+
+const CartContext = createContext();
+
+export function CartProvider({ children }) {
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (item) => {
+    setCart([...cart, item]);
+  };
+
+  const removeFromCart = (index) => {
+    const updated = [...cart];
+    updated.splice(index, 1);
+    setCart(updated);
+  };
+
+  const clearCart = () => {
+    setCart([]);
+  };
+
+  return (
+    <CartContext.Provider value={{ cart, addToCart, removeFromCart, clearCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+}
+
+export const useCart = () => useContext(CartContext);
